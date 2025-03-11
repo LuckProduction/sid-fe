82d8f4090c84d622220fbf204907c235e2ca55e5
adilasomaaa <yasdilofficial@gmail.com>
2025-03-11 23:22:39 +0800

    feat(ci-cd):fixall

--- src/utils/asset.js
+++ src/utils/asset.js
@@ -1,7 +1,9 @@
 const baseUrl = import.meta.env.VITE_BASE_URL;
 const tenant = import.meta.env.VITE_TENANTS;
-export const BASE_URL = baseUrl.replace('://', `://${tenant}.`) + '/storage/';
+const [protocol, host] = baseUrl.split('://');
+export const BASE_URL = `${protocol}://${tenant}.${host}/storage/`;
 
 export default function asset(url) {
   return BASE_URL + url;
 }
+
